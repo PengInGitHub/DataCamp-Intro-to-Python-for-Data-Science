fcb373f852b8e6313f4556eeb7a6d877fb173dbd
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-13 16:21:22 +0100

    slice a list, take a range of it

--- Python Lists/script.py
+++ Python Lists/script.py
@@ -36,9 +36,12 @@ print(x[-2])
 #subset and calculate
 print(x[0]+x[2])
 
-
-
-
+#slicing and dicing
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+downstairs=areas[0:6]
+print(downstairs)
+upstairs=areas[6:10]
+print(upstairs)
 
 
 
