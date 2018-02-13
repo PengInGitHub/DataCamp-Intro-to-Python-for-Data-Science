650ee1f1e12b6c6533bf9c2d99a98eb04bda9dcf
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-13 15:41:00 +0100

    create list

--- Python Lists/script.py
+++ Python Lists/script.py
@@ -1,2 +1,12 @@
 #Create a List
 #list is a compound data type, you can group values together
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+#create a list areas
+areas=[hall,kit,liv,bed,bath]
+print(areas)
