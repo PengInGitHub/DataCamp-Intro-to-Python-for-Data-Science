64a95810ba9b84b28553c8a97e25c6da2dc7dee7
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-13 09:12:02 +0100

    add Python as calculator

--- Python Basics/script.py
+++ Python Basics/script.py
@@ -4,3 +4,9 @@ print(5/8)
 
 #Addition works too
 print(7+10)
+
+
+
+#Python as a calculator
+#basic: addition, subtraction, multiplication and division
+#advanced: exponentation - **, % - returns reminder,  
