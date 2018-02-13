1f8ce595168072fcc19cd9223ac84c890064da85
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-13 17:15:16 +0100

    make a copy of created list

--- Python Lists/script.py
+++ Python Lists/script.py
@@ -90,8 +90,13 @@ print(x,areas_1)
 
 
 #Inner workings of list
+
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+#create areas_copy
+areas_copy=areas[:]
+areas_copy=list(areas)
 
 
 
